16fdb68efcf1f865844dc44818c0470cf9793a93
unknown <edesbalazs@gmail.com>
2024-11-08 23:40:07 +0100

    better svg loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svg from 'vite-svg-loader'
 import { alphaTab } from '@coderline/alphatab/vite'
 
 export default defineConfig({
-  plugins: [react(), alphaTab()],
+  plugins: [react(), alphaTab(), svg({ defaultImport: 'url' })],
   appType: 'spa',
   base: './',
   build: {
